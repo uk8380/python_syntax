6d67d70937c4afc2d67df313e08c634bb05f8e83
mannepalli uday kumar <65896248+uk8380@users.noreply.github.com>
2021-06-15 14:30:19 +0530

    Add files via upload

---
+++ about-basic.py
@@ -0,0 +1,13 @@
+#Python is a popular programming language.
+#It was created by Guido van Rossum, and released in 1991.
+#syntax
+print("hello world!")
+if(5>2):
+    print("hi")
+x=10
+y=5
+s=x+y
+print(s)
+q=x*y
+print(q)
+uk=3.10
